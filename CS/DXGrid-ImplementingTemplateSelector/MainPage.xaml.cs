df99c94ce02ee1343e9b6242d8c0d4830e09a1f5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid-ImplementingTemplateSelector/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+
+namespace DXGrid_ImplementingTemplateSelector {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = new ProductList();
+        }
+    }
+    public class RowCellTemplateSelector : DataTemplateSelector {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container) {
+            GridCellData cellData = item as GridCellData;
+            FrameworkElement presenter = container as FrameworkElement;
+            if (cellData != null && presenter != null) {
+                if (cellData.Column.FieldName != "UnitPrice")
+                    return base.SelectTemplate(item, container);
+                if(Convert.ToDouble(cellData.Value) > 20)
+                    return RowCellTemplate1;
+                else
+                    return RowCellTemplate2;
+            }
+            return base.SelectTemplate(item, container);
+        }
+        public DataTemplate RowCellTemplate1 { get; set; }
+        public DataTemplate RowCellTemplate2 { get; set; }
+    }
+}
